6f81a3f96b68acf0a7f8cde56ed8055bd975a0a3
marsjava <marikannan.cvp@gmail.com>
2018-02-12 10:22:59 +0530

    FirstDeploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Eyeopen Technologies, Welcome to All Again.';
 }
